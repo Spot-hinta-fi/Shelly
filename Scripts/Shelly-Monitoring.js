29f2f47eb040b2459ddb346413a39012bb109b41
Spot-hinta.fi <119565445+Spot-hinta-fi@users.noreply.github.com>
2023-01-13 07:12:27 +0200

    Add scripts to new folder

---
+++ Scripts/Shelly-Monitoring.js
@@ -0,0 +1,104 @@
+/* More information about the API's (in Finnish): https://spot-hinta.fi/
+ * Support API development and maintenance: https://www.buymeacoffee.com/spothintafi
+ * 
+ * This script ise used to monitor other script execution and when needed restarts the scripts.
+ * Scripts that do HTTP requests can sometimes fail, but this script will notice it and restart.
+ * Also Internet connection can be monitored and Shelly rebooted if connection is lost. */
+
+// *****************************
+// SETTINGS - Change when needed
+// *****************************
+
+let SETTINGS = {
+    MonitorScript_1: true,  // Change value to true/false, depending if you want to monitor script 1
+    MonitoredScript_1: "1", // Define here the ID of the script to be monitored
+    MonitorScript_2: false,  // Change value to true/false, depending if you want to monitor script 2
+    MonitoredScript_2: "2", // Define here the ID of the script to be monitored
+    MonitorInternetConnection: false,  // Set this true, if you want to reboot shelly when Internet connection is lost
+    NumberOfFailedRoundsToRebootShelly: 5,  // How many timer rounds are waited before reboot happens.
+};
+
+// **************************************
+// MAIN SCRIPT - NO NEED TO TOUCH USUALLY
+// **************************************
+
+// Default timer round is 60 seconds
+Timer.set(60000, true, function (ud) {
+    MonitorScriptExecution();
+    MonitorInternetConnection();
+}, null); 
+
+
+function MonitorScriptExecution()
+{
+    // Monitoring script 1
+    if (SETTINGS.MonitorScript_1 === true)
+    {
+        print("Monitoring script 1 execution, script ID to monitor: " + SETTINGS.MonitoredScript_1);
+
+        Shelly.call("Script.GetStatus", { id: SETTINGS.MonitoredScript_1 }, function (res)
+        {
+            if (res.running === true)
+            {
+                print("Monitored script 1 is running. All good.");
+            }
+            else
+            {
+                print("Script 1 is not running. Starting the script.");
+                Shelly.call("Script.Start", { id: SETTINGS.MonitoredScript_1 }, null, null);
+            };
+        }, null);
+    }
+
+    // Monitoring script 2
+    if (SETTINGS.MonitorScript_2 === true)
+    {
+        print("Monitoring script 2 execution, script ID to monitor: " + SETTINGS.MonitoredScript_2);
+
+        Shelly.call("Script.GetStatus", { id: SETTINGS.MonitoredScript_2 }, function (res)
+        {
+            if (res.running === true)
+            {
+                print("Monitored script 2 is running. All good.");
+            }
+            else
+            {
+                print("Script 2 is not running. Starting the script.");
+                Shelly.call("Script.Start", { id: SETTINGS.MonitoredScript_2 }, null, null);
+            };
+        }, null);
+    }
+}
+
+// Monitor Internet Connection
+let failedRounds = 0;
+function MonitorInternetConnection()
+{
+    if (SETTINGS.MonitorInternetConnection === false) {
+        return;
+    }
+
+    Shelly.call("Shelly.GetStatus", "", function (res)
+    {
+
+        // TODO: are there better ways to check the Internet connection?
+        if (res.wifi.status === "got ip" && res.cloud.connected === true)
+        {
+            failedRounds = 0;
+            print("Internet connection is working and connected to the Shelly cloud");
+            return;
+        }
+
+        failedRounds += 1;
+        print("Internet connection is NOT working. Failed rounds now:");
+        print(failedRounds);
+
+        if (failedRounds >= SETTINGS.NumberOfFailedRoundsToRebootShelly)
+        {
+            failedRounds = 0;
+            print("Rebooting Shelly because Internet connection does not work!");
+            Shelly.call("Shelly.Reboot");
+        }
+    }, null);
+}
+
---
+++ Scripts/Shelly-Outdoor_temperature_adjusted_heating.js
@@ -0,0 +1,226 @@
+/* More information about the API's (in Finnish): https://spot-hinta.fi/
+ * Support API development and maintenance: https://www.buymeacoffee.com/spothintafi
+ * 
+ * With this script, it is possible to control up to three relays according temporature adjusted 'rank' */
+
+
+// ***********************************
+// SETTINGS for RELAY 1 - change these
+// ***********************************
+let SETTINGS_1 =
+{
+    RelayIsInUse: false, // Change this to true/false depending if you want to use this relay or not
+    RankAtZeroDegrees: "5", // "Rank" (number of cheapest hours) when outdoor temperature is 0°C
+    RankAdjusterPercentage: "15", // Percentage how much "Rank" is adjusted when outdoor temperature changes by one degree
+    MinimumRank: "3", // Minimum 'Rank' when temperature goes above zero and Rank is getting smaller
+    PriceAlwaysAllowed: "3", // "Allow always cheap prices". Price when relay is always ON.
+    PostalCode: "00100", // Postal code (Finnish), which area temperature is used in calculations
+    BackupHours: ["00", "01", "02", "03", "20", "21"],  // Backup hours if API is not answering or Internet connection is down.
+    BoosterHours: "99,99", // During these hours relay is always ON. If you don't want this, use "99,99"
+    PriorityHours: "99,99", // List here hours you want to prioritize. With PriceModifier: "0", these hours always get the smallest 'rank'
+    PriceModifier: "-2,50", // Put here the difference in Euro cents if priority hours have lower price, like 'night electricity'
+    Relay: "0",  // Number of the relay within Shelly. Make sure this is correct
+    RelayName: "Bathroom floor",  // Whatever name for this relay. Used in debug logging mostly.
+    Inverted: false, // If "true", relay logic is inverted
+};
+
+// ***********************************
+// SETTINGS for RELAY 2 - change these
+// ***********************************
+let SETTINGS_2 =
+{
+    RelayIsInUse: false, // Change this to true/false depending if you want to use this relay or not
+    RankAtZeroDegrees: "5", // "Rank" (number of cheapest hours) when outdoor temperature is 0°C
+    RankAdjusterPercentage: "15", // Percentage how much "Rank" is adjusted when outdoor temperature changes by one degree
+    MinimumRank: "3", // Minimum 'Rank' when temperature goes above zero and Rank is getting smaller
+    PriceAlwaysAllowed: "3", // "Allow always cheap prices". Price when relay is always ON.
+    PostalCode: "00100", // Postal code (Finnish), which area temperature is used in calculations
+    BackupHours: ["00", "01", "02", "03", "20", "21"],  // Backup hours if API is not answering or Internet connection is down.
+    BoosterHours: "99,99", // During these hours relay is always ON. If you don't want this, use "99,99"
+    PriorityHours: "99,99", // List here hours you want to prioritize. With PriceModifier: "0", these hours always get the smallest 'rank'
+    PriceModifier: "-2,50", // Put here the difference in Euro cents if priority hours have lower price, like 'night electricity'
+    Relay: "0",  // Number of the relay within Shelly. Make sure this is correct
+    RelayName: "Big boiler",  // Whatever name for this relay. Used in debug logging mostly.
+    Inverted: false, // If "true", relay logic is inverted
+};
+
+// ***********************************
+// SETTINGS for RELAY 3 - change these
+// ***********************************
+let SETTINGS_3 =
+{
+    RelayIsInUse: false, // Change this to true/false depending if you want to use this relay or not
+    RankAtZeroDegrees: "5", // "Rank" (number of cheapest hours) when outdoor temperature is 0°C
+    RankAdjusterPercentage: "15", // Percentage how much "Rank" is adjusted when outdoor temperature changes by one degree
+    MinimumRank: "3", // Minimum 'Rank' when temperature goes above zero and Rank is getting smaller
+    PriceAlwaysAllowed: "3", // "Allow always cheap prices". Price when relay is always ON.
+    PostalCode: "00100", // Postal code (Finnish), which area temperature is used in calculations
+    BackupHours: ["00", "01", "02", "03", "20", "21"],  // Backup hours if API is not answering or Internet connection is down.
+    BoosterHours: "99,99", // During these hours relay is always ON. If you don't want this, use "99,99"
+    PriorityHours: "99,99", // List here hours you want to prioritize. With PriceModifier: "0", these hours always get the smallest 'rank'
+    PriceModifier: "-2,50", // Put here the difference in Euro cents if priority hours have lower price, like 'night electricity'
+    Relay: "0",  // Number of the relay within Shelly. Make sure this is correct
+    RelayName: "Livingroom",  // Whatever name for this relay. Used in debug logging mostly.
+    Inverted: false, // If "true", relay logic is inverted
+};
+
+
+// **************************************
+// MAIN SCRIPT - NO NEED TO TOUCH USUALLY
+// **************************************
+
+// Variables needed to control execution
+let currentHour = "";
+let Relay_1_Executed = false;
+let Relay_2_Executed = false;
+let Relay_3_Executed = false;
+
+// Main timer, which calls the API to decide actions on relays
+// Only one successful execution per hour per relay is done.
+Timer.set(60000, true, function (ud) {
+
+    // Calling the status to get the current time
+    Shelly.call("Shelly.GetStatus", "", function (res) {
+
+        // Check if hour has changed
+        let hour = res.sys.time.slice(0, 2); // f.ex. "21:34"
+        if (currentHour !== hour) {
+            currentHour = hour;
+
+            // Force relay "executed" if it is not in use
+            if (SETTINGS_1.RelayIsInUse === true) { Relay_1_Executed = false } else { Relay_1_Executed = true; };
+            if (SETTINGS_2.RelayIsInUse === true) { Relay_2_Executed = false } else { Relay_2_Executed = true; };
+            if (SETTINGS_3.RelayIsInUse === true) { Relay_3_Executed = false } else { Relay_3_Executed = true; };
+        }
+
+        // Do not run anymore if execution has been successfull for all relays
+        if (Relay_1_Executed === true && Relay_2_Executed === true && Relay_3_Executed === true) {
+            print("Already executed this hour successfully.");
+            return;
+        }
+
+        // First relay control is executed
+        if (Relay_1_Executed === false) {
+
+            let urlToCall = GetDynamicUrl(SETTINGS_1);
+            print("URL to call (dynamic 1): " + urlToCall);
+
+            Shelly.call("HTTP.GET", { url: urlToCall, timeout: 15, ssl_ca: "*" }, function (res, error_code, error_msg, ud) {
+                print("Performing control for relay: " + SETTINGS_1.RelayName);
+                let result = RunResponse(error_code, error_msg, res.code,
+                    SETTINGS_1.Relay, SETTINGS_1.RelayName, SETTINGS_1.BackupHours, SETTINGS_1.Inverted);
+                if (result === true) Relay_1_Executed = true;
+            }, null);
+        }
+
+        // Second relay control is executed
+        if (Relay_2_Executed === false) {
+
+            let urlToCall = GetDynamicUrl(SETTINGS_2);
+            print("URL to call (dynamic 2): " + urlToCall);
+
+            Shelly.call("HTTP.GET", { url: urlToCall, timeout: 15, ssl_ca: "*" }, function (res, error_code, error_msg, ud) {
+                print("Performing control for relay: " + SETTINGS_2.RelayName);
+                let result = RunResponse(error_code, error_msg, res.code,
+                    SETTINGS_2.Relay, SETTINGS_2.RelayName, SETTINGS_2.BackupHours, SETTINGS_2.Inverted);
+                if (result === true) Relay_2_Executed = true;
+            }, null);
+        }
+
+        // Third relay control is executed
+        if (Relay_3_Executed === false) {
+
+            let urlToCall = GetDynamicUrl(SETTINGS_3);
+            print("URL to call (dynamic 3): " + urlToCall);
+
+            Shelly.call("HTTP.GET", { url: urlToCall, timeout: 15, ssl_ca: "*" }, function (res, error_code, error_msg, ud) {
+                print("Performing control for relay: " + SETTINGS_3.RelayName);
+                let result = RunResponse(error_code, error_msg, res.code,
+                    SETTINGS_3.Relay, SETTINGS_3.RelayName, SETTINGS_3.BackupHours, SETTINGS_3.Inverted);
+                if (result === true) Relay_3_Executed = true;
+            }, null);
+        }
+    }, null);
+}, null);
+
+
+// This controls the relay actions based on the result from the API call
+function RunResponse(errorCode, errorMessage, responseCode, relay, relayName, backupHours, inverted) {
+    // Network errors
+    if (errorCode !== 0) {
+        print("Network error occurred: " + errorMessage);
+        print(errorCode);
+        RunBackupHourRule(backupHours, relay, relayName, inverted);
+        return false;
+    }
+
+    if (responseCode === 200) {
+        if (inverted === true) {
+            print("Relay '" + relayName + "' OFF (INVERTED)");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:false}", null, null); // Relay OFF
+            return true;
+        } else {
+            print("Relay '" + relayName + "' ON");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:true}", null, null); // Relay ON
+            return true;
+        }
+    }
+    else if (responseCode === 400) {
+        if (inverted === true) {
+            print("Relay '" + relayName + "' ON (INVERTED)");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:true}", null, null); // Relay ON
+            return true;
+        } else {
+            print("Relay '" + relayName + "' OFF");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:false}", null, null); // Relay OFF
+            return true;
+        }
+    }
+    else {
+        print("Executing backup rule for relay: " + relayName);
+        RunBackupHourRule(backupHours, relay, relayName, inverted);
+        return false;
+    };
+}
+
+// This is executed if API did not respond properly. This is NOT considered as successful execution
+function RunBackupHourRule(backupHours, relay, relayName, inverted) {
+    let currentHourIsListed = false;
+    for (let i = 0; i < backupHours.length; i++) {
+        if (backupHours[i] === currentHour) { currentHourIsListed = true; }
+    }
+
+    if (currentHourIsListed) {
+        if (inverted === true) {
+            print("Error situation. Turn relay '" + relayName + "' OFF during backup hours. (INVERTED)");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:false}", null, null); // Error situation. Relay OFF.
+        } else {
+            print("Error situation. Turn relay '" + relayName + "' ON during backup hours.");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:true}", null, null); // Error situation. Relay ON.
+        }
+    }
+    else {
+        if (inverted === true) {
+            print("Error situation. Turn relay '" + relayName + "' ON outside backup hours. (INVERTED)");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:true}", null, null); // Error situation. Relay ON.
+
+        } else {
+            print("Error situation. Turn relay '" + relayName + "' OFF outside backup hours");
+            Shelly.call("Switch.Set", "{ id:" + relay + ", on:false}", null, null); // Error situation. Relay OFF.
+        }
+    }
+}
+
+// Builds URL to call the API
+function GetDynamicUrl(settingsNow) {
+    let url = "https://api.spot-hinta.fi/JustNowRankDynamic";
+    url += "?rankAtZeroDegrees=" + settingsNow.RankAtZeroDegrees;
+    url += "&rankAdjusterPercentage=" + settingsNow.RankAdjusterPercentage;
+    url += "&minimumRank=" + settingsNow.MinimumRank;
+    url += "&priceAlwaysAllowed=" + settingsNow.PriceAlwaysAllowed;
+    url += "&postalCode=" + settingsNow.PostalCode;
+    url += "&boosterHours=" + settingsNow.BoosterHours;
+    url += "&priorityHours=" + settingsNow.PriorityHours;
+    url += "&priceModifier=" + settingsNow.PriceModifier;
+    return url;
+}
