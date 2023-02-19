5c314d1dd0705cb4519b54939c70297b76d62fa0
frspp <22942340+frspp@users.noreply.github.com>
2023-02-19 10:00:20 +0200

    Update Shelly-Minimal_water_heating.js
    some comments

--- Scripts/Shelly-Minimal_water_heating.js
+++ Scripts/Shelly-Minimal_water_heating.js
@@ -1,32 +1,40 @@
 /* Spot-hinta.fi service is a privately funded service. If you want to support it, you can do it here:
 * https://www.buymeacoffee.com/spothintafi  -- Thank you! */
 
-/* Parameters. Modify these as per needed. */
-let Region = "FI"; // See all supported regions from Swagger documentation: https://api.spot-hinta.fi/swagger/ui
+// Settings 
+let Region = "FI"; // See supported regions in Swagger documentation: https://api.spot-hinta.fi/swagger/ui
 let Rank = "4"; // How many hours relay is on (cheapest hours) 
 let PriceAllowed = "3"; // Heating is always on, when price is below this (Euro cents)
 let PriorityHours = "00,01,02,03,04,05,06"; // These hours are prioritized (smallest ranks to these hours, f.ex. if you want to heat boiler during night). Use "99", if not wanted.
 let PriorityHoursRank = "3";  // This limits how many hours are prioritized (i.e. 3 cheapest hours from priority hours)
 let BackupHours = ["03", "04", "05", "06"]; // If API or Internet connection is down, heat these hours
 let BoosterHours = "04,17"; // Run early morning one hour and afternoon one hour. Use "99", if not wanted.
+let interval = 1 * 60 * 1000; // How often run this script? First number is the minutes. Minutes * 60 sec * 1000 millisec
 
-// SCRIPT, dont edit below
+// Script
+// Create variables
 let cHour = ""; let bhour = false; let Executed = false; let rclosed = false;
+
+// Create variable for API call
 let urlToCall = "https://api.spot-hinta.fi/JustNowRank/" + Rank + "/" + PriceAllowed;
 urlToCall += "?boosterHours=" + BoosterHours + "&priorityHours=" + PriorityHours;
 urlToCall += "&priorityHoursRank=" + PriorityHoursRank + "&region=" + Region;
 
-Timer.set(60000, true, function () {
+// Create timer
+Timer.set(interval, true, function () {
     Shelly.call("Shelly.GetStatus", "", function (res) {
         let hour = res.sys.time.slice(0, 2); // f.ex. "21:34"
         if (cHour !== hour) { cHour = hour; Executed = false; }
-        if (Executed === true) { print("Already executed this hour successfully."); return; }
+        if (Executed === true) { print("Current hour is already done."); return; }
         bhour = false; for (let i = 0; i < BackupHours.length; i++) { if (BackupHours[i] === cHour) { bhour = true; } }
         print("URL to call: " + urlToCall);
+
+        // Send HTTP GET to API
         Shelly.call("HTTP.GET", { url: urlToCall, timeout: 15, ssl_ca: "*" }, RunResponse);
     });
 });
 
+// Check the API response and change the relay accordingly 
 function RunResponse(result) {
     if (result.code === 400 && rclosed === true) { print("Relay already turned OFF by this script."); Executed = true; return; } // Allows possible other script to control relay while this is closed
     if (result.code === 400 && rclosed === false) { Shelly.call("Switch.Set", "{ id:0, on:false}", null, null); print("Relay OFF"); rclosed = true; Executed = true; return; }
