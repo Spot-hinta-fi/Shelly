d12ac5e4719c309be6ba2c85fd4fb4cd5962a0b3
Spot-hinta.fi <info@spot-hinta.fi>
2023-11-09 16:41:25 +0200

    Street light price limit implementation.

--- Scripts/Shelly-PlusPlugS-StreetLight.js
+++ Scripts/Shelly-PlusPlugS-StreetLight.js
@@ -1,5 +1,5 @@
 // You can support spot-hinta.fi service here: https://www.buymeacoffee.com/spothintafi
-// Supported Shelly firmwares: 1.0.3 - 1.0.8. Script version: 2023-11-08
+// Supported Shelly firmwares: 1.0.3 - 1.0.8. Script version: 2023-11-09
 
 // NOTE! This script works only with "Shelly Plus Plug S" -smart plug
 
@@ -7,14 +7,27 @@
 let Region = "FI"; // Supported regions: DK1, DK2, EE, FI, LT, LV, NO1, NO2, NO3, NO4, NO5, SE1, SE2, SE3, SE4
 
 // Configure colors
-const CheapPriceColor = [0, 100, 0]; // Cheapest 8 hours == Green
-const MiddlePriceColor = [100, 60, 0]; // Middle price 8 hours == Yellowish
-const ExpensivePriceColor = [100, 0, 0]; // Most expensive hours == Red
-const UnknownPriceColor = [100, 100, 100]; // Could not find price information == White
+const CheapPriceColor = [0, 100, 0]; // Green
+const MiddlePriceColor = [100, 60, 0]; // Yellow
+const ExpensivePriceColor = [100, 0, 0]; // Red
+const UnknownPriceColor = [100, 100, 100]; // White
 
-// Script starts here, do not edit.
+// Customized price limits. Overrides the standard functionality of 8 hours relative blocks.
+// Define cheap price limit (green) and expensive price limit (red). What is left between is middle price (yellow).
+const UsePriceLimits = false; // Change to 'true' if you want to define price limits for colors
+const CheapPriceLimit = 5;  // Cheap price limit in euro cents
+const ExpensivePriceLimit = 12; // Expensive price limit in euro cents
+
+
+// Script starts here, do not edit
 print("PlusPlugS-StreetLight script is starting... (color will be set in 60 seconds)");
-let config; let currentHour = -1; let currentHourColor = UnknownPriceColor;
+let config; let currentHour = -1; let currentHourColor = UnknownPriceColor; let urlToCall = "";
+
+if (UsePriceLimits === true) {
+    urlToCall = "https://api.spot-hinta.fi/JustNow/" + CheapPriceLimit + "/" + ExpensivePriceLimit + "?region=" + Region
+} else {
+    urlToCall = "https://api.spot-hinta.fi/JustNowRank?region=" + Region;
+}
 
 // Get current configuration. Only color is modified, other settings remain.
 Shelly.call("PLUGS_UI.GetConfig", null, function (response) { config = response; });
@@ -25,19 +38,28 @@ Timer.set(60000, true, function () {
     else {
         currentHour = new Date().getHours();
         print("Hour has changed, getting what color the LED light should be...");
-        let urlToCall = "https://api.spot-hinta.fi/JustNowRank?region=" + Region;
         Shelly.call("HTTP.Request", { method: "GET", url: urlToCall, timeout: 10, ssl_ca: "*" }, ProcessResponse);
     }
 });
 
 function ProcessResponse(response, error_code) {
     if (error_code === 0 && response !== null && response.code === 200) {
-        if (response.body <= 8) { print("Hour is cheap. Rank: " + response.body); currentHourColor = CheapPriceColor; }
-        else if (response.body >= 17) { print("Hour is expensive. Rank: " + response.body); currentHourColor = ExpensivePriceColor; }
-        else { print("Hour is middle price. Rank: " + response.body); currentHourColor = MiddlePriceColor; }
-        ChangeColor(currentHourColor);
+        if (UsePriceLimits === true) {
+            if (response.body === 0) { print("Hour is cheap"); currentHourColor = CheapPriceColor; }
+            if (response.body === 1) { print("Hour is middle price"); currentHourColor = MiddlePriceColor; }
+            if (response.body === 2) { print("Hour is expensive"); currentHourColor = ExpensivePriceColor; }
+            ChangeColor(currentHourColor);
+        }
+        else {
+            if (response.body <= 8) { print("Hour is cheap. Rank: " + response.body); currentHourColor = CheapPriceColor; }
+            else if (response.body >= 17) { print("Hour is expensive. Rank: " + response.body); currentHourColor = ExpensivePriceColor; }
+            else { print("Hour is middle price. Rank: " + response.body); currentHourColor = MiddlePriceColor; }
+            ChangeColor(currentHourColor);
+        }
     }
-    else { print("An error occurred while fetching rank data"); ChangeColor(UnknownPriceColor); currentHour = -1; }
+    else {
+        print("An error occurred while fetching rank data"); ChangeColor(UnknownPriceColor); currentHour = -1;
+    }
 }
 
 function ChangeColor(color) {
@@ -55,4 +77,5 @@ function ProcessColorChangeResponse(response, error_code, error_msg) {
     }
 
     print("Color change was not successful. Error code: " + error_code + " - Error message: " + error_msg);
+    currentHour = -1;
 }
