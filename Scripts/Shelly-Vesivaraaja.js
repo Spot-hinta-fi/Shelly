8d4064f23ee23836227a798d7e7e549a2aa45bc4
Spot-hinta.fi <info@spot-hinta.fi>
2024-01-19 21:42:53 +0200

    Improved and further simplified code. Thanks to Heikki Vesalainen for his contribution here.

--- Scripts/Shelly-Vesivaraaja.js
+++ Scripts/Shelly-Vesivaraaja.js
@@ -3,20 +3,18 @@
 
 // Asetukset lämminvesivaraajan ohjaukseen
 let Lammitystunnit_yo = 3; // Kuinka monta halvinta tuntia lämmitetään 22:00 - 06:59?
-let Lammitystunnit_iltapaiva = 0;  // Kuinka monta halvinta tuntia lämmitetään 12:00 - 19:59?
-let Rele = 0; // Mitä relettä ohjataan? (0 on ensimmmäinen rele Shellyssä. "Switch Add-on" moduulin rele on 100)
+let Lammitystunnit_iltapaiva = 10;  // Kuinka monta halvinta tuntia lämmitetään 12:00 - 19:59?
+let Rele = 0; // Mitä relettä ohjataan? (0 on ensimmmäinen rele Shellyssä. "Switch Add-on" rele on 100)
 
 // Koodi - älä koske
-let hour = -1; let executed = false; let url = "https://api.spot-hinta.fi/WaterBoiler/" + Lammitystunnit_yo + "/" + Lammitystunnit_iltapaiva;
+let url = "https://api.spot-hinta.fi/WaterBoiler/" + Lammitystunnit_yo + "/" + Lammitystunnit_iltapaiva; let hour = null;
 print("WaterBoiler: Ohjaus käynnistyy. Ensimmäinen ohjaus tapahtuu 30 sekunnin kuluttua.");
 Timer.set(30000, true, function () {
-    if (hour != new Date().getHours()) { executed = false; hour = new Date().getHours(); }
-    if (executed) { print("WaterBoiler: Odotetaan tunnin vaihtumista"); return; }
-    Shelly.call("HTTP.GET", { url: url, timeout: 15, ssl_ca: "*" }, function (result, error_code) {
-        if (error_code !== 0 || result == null) { ShellyCall(true); executed = false; print("WaterBoiler: Kytketty päälle (virhetilanne)"); return; }
-        if (result.code == 200) { ShellyCall(true); executed = true; print("WaterBoiler: Kytketty päälle."); return; }
-        if (result.code == 400) { ShellyCall(false); executed = true; print("WaterBoiler: Kytketty pois päältä."); return; }
-        if (result.code !== 400 && result.code !== 200) { ShellyCall(true); executed = false; print("WaterBoiler: Kytketty päälle (virhetilanne)"); return; }
+    if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista"); return; }
+    Shelly.call("HTTP.GET", { url: url, timeout: 15, ssl_ca: "*" }, function (res, err) {
+        hour = (err != 0 || res == null || (res.code !== 200 && res.code !== 400)) ? null : new Date().getHours();
+        let on = hour ? res.code != 400 : true;
+        Shelly.call("Switch.Set", "{ id:" + Rele + ", on:" + on + "}", null, null);
+        print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä") + (hour ? "." : " (virhetilanne)."));
     });
 });
-function ShellyCall(status) { Shelly.call("Switch.Set", "{ id:" + Rele + ", on:"+ status +"}", null, null); }
