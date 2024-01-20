0ef7ced97432ab4dbaaafa9ec5ed3fbe56113797
Spot-hinta.fi <info@spot-hinta.fi>
2024-01-20 09:34:18 +0200

    Shorter comments and setting variable names

--- Scripts/Shelly-Vesivaraaja.js
+++ Scripts/Shelly-Vesivaraaja.js
@@ -1,20 +1,20 @@
-﻿// Voit halutessasi tukea palvelun ylläpitoa täällä: https://www.buymeacoffee.com/spothintafi
-// Tuetut Shelly laiteohjelmistoversiot: 1.0.3 - 1.1.0. Skriptin versio: 2024-01-19
+﻿// Kiitos tuestasi: https://www.buymeacoffee.com/spothintafi
+// Tuetut Shelly ohjelmistot: 1.0.3 - 1.1.0. Skriptin versio: 2024-01-19
 
-// Asetukset lämminvesivaraajan ohjaukseen
-let Lammitystunnit_yo = 3; // Kuinka monta halvinta tuntia lämmitetään 22:00 - 06:59?
-let Lammitystunnit_iltapaiva = 0;  // Kuinka monta halvinta tuntia lämmitetään 12:00 - 19:59?
-let Rele = 0; // Mitä relettä ohjataan? (0 on ensimmmäinen rele Shellyssä. "Switch Add-on" rele on 100)
+// ASETUKSET
+let TUNNIT_YO = 3; // Yötuntien lukumäärä 22:00 - 06:59?
+let TUNNIT_IP = 0; // Iltapäivätuntien lukumäärä 12:00 - 19:59?
+let RELE = 0;      // Mitä relettä ohjataan? 
 
-// Koodi - älä koske
-let url = "https://api.spot-hinta.fi/WaterBoiler/" + Lammitystunnit_yo + "/" + Lammitystunnit_iltapaiva; let hour = null;
+// KOODI
+let url = "https://api.spot-hinta.fi/WaterBoiler/" + TUNNIT_YO + "/" + TUNNIT_IP; let hour = null;
 print("WaterBoiler: Ohjaus käynnistyy. Ensimmäinen ohjaus tapahtuu 30 sekunnin kuluttua.");
 Timer.set(30000, true, function () {
-    if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista"); return; }
+    if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista."); return; }
     Shelly.call("HTTP.GET", { url: url, timeout: 15, ssl_ca: "*" }, function (res, err) {
         hour = (err != 0 || res == null || (res.code !== 200 && res.code !== 400)) ? null : new Date().getHours();
         let on = hour ? res.code != 400 : true;
-        Shelly.call("Switch.Set", "{ id:" + Rele + ", on:" + on + "}", null, null);
+        Shelly.call("Switch.Set", "{ id:" + RELE + ", on:" + on + "}", null, null);
         print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä") + (hour ? "." : " (virhetilanne)."));
     });
 });
