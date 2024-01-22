ccf447c04141d18f32a1b6a43a0bedbc032b70e4
Spot-hinta.fi <info@spot-hinta.fi>
2024-01-23 00:37:54 +0200

    Small bug fix to Shelly-vesivaraaja.js" script. The first hour of the day went always on despite the price. Problem was how Shelly javascript engine handles ?-check (0 and null behave the same).

--- Scripts/Shelly-Vesivaraaja.js
+++ Scripts/Shelly-Vesivaraaja.js
@@ -7,14 +7,14 @@ let TUNNIT_IP = 0; // Iltapäivätuntien lukumäärä 12:00 - 19:59?
 let RELE = 0;      // Mitä relettä ohjataan? 
 
 // KOODI
-let url = "https://api.spot-hinta.fi/WaterBoiler/" + TUNNIT_YO + "/" + TUNNIT_IP; let hour = null;
+let url = "https://api.spot-hinta.fi/WaterBoiler/" + TUNNIT_YO + "/" + TUNNIT_IP; let hour = -1;
 print("WaterBoiler: Ohjaus käynnistyy 30 sekunnissa.");
 Timer.set(30000, true, function () {
     if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista."); return; }
     Shelly.call("HTTP.GET", { url: url, timeout: 15, ssl_ca: "*" }, function (res, err) {
-        hour = (err != 0 || res == null || (res.code !== 200 && res.code !== 400)) ? null : new Date().getHours();
-        let on = hour ? res.code != 400 : true;
+        hour = (err != 0 || res == null || (res.code !== 200 && res.code !== 400)) ? -1 : new Date().getHours();
+        let on = (hour > -1) ? res.code != 400 : true;
         Shelly.call("Switch.Set", "{ id:" + RELE + ", on:" + on + "}", null, null);
-        print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä") + (hour ? "." : " (virhetilanne)."));
+        print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä") + ((hour > -1) ? "." : " (virhetilanne)."));
     });
 });
