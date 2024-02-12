f6d7b0da9911dc3728e9280e54aab976ad2b2a93
Spot-hinta.fi <info@spot-hinta.fi>
2024-02-12 23:27:20 +0200

    Bugfix to current hour getting.

--- Scripts/Shelly-Pikakoodi.js
+++ Scripts/Shelly-Pikakoodi.js
@@ -7,7 +7,7 @@ let Pikakoodit = [103, 999, 999]; // Hae sopivat pikakoodit täältä: https://s
 // SKRIPTI. Älä muokkaa alla olevaa
 let rr = -1; let tunti = -1; let vtila = false; print("Pikakoodi: Skripti käynnistyy.");
 Timer.set(15000, true, function () {
-    if (tunti == new Date().GetHours) { return; } else {
+    if (tunti == new Date().getHours()) { return; } else {
         rr--; rr = (rr < 0) ? Pikakoodit.length - 1 : rr; if (Pikakoodit[rr] == 999) { return; }
         Shelly.call("HTTP.GET", { url: "https://api.spot-hinta.fi/QuickCode/" + Pikakoodit[rr], timeout: 5, ssl_ca: "*" }, function (res, err) {
             let tila = (err == 0 && res != null && res.code == 400) ? false : true;
@@ -15,7 +15,7 @@ Timer.set(15000, true, function () {
             Shelly.call("Switch.Set", "{ id:" + rr + ", on:" + tila + "}", null, null);
             print("Pikakoodi: Rele " + rr + " on kytketty " + (tila ? "päälle" : "pois päältä") + (!virhe ? "." : " (virhetilanne)."));
             if (virhe && res != null && res.body != null) { print("Pikakoodi: Rele " + rr + " virheviesti: " + res.body) }
-            if (rr == 0) { tunti = (vtila) ? -1 : new Date().GetHours; print("Pikakoodi: Tunti suoritettu?  " + !vtila); vtila = (tunti > -1) ? false : true; }
+            if (rr == 0) { tunti = (vtila) ? -1 : new Date().getHours(); print("Pikakoodi: Tunti suoritettu?  " + !vtila); vtila = (tunti > -1) ? false : true; }
         });
     }
 });
