336976f8a71c951c84ae50793c736c1d62556faa
Lehto, Mika <mikal@basware.com>
2024-09-09 18:14:41 +0300

    Organized scripts a bit. Moved two "unofficial" scripts under the "Extras" folder.  Updated "Vesivaraaja-yosiirto" script to have night hours in a configurable variable. Updated script dates.

--- Scripts/Shelly-Vesivaraaja-yosiirto.js
+++ Scripts/Shelly-Vesivaraaja-yosiirto.js
@@ -1,14 +1,15 @@
 ﻿// Kiitos tuestasi: https://www.buymeacoffee.com/spothintafi
-// Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.2. Skriptin versio: 2024-09-07
+// Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.2. Skriptin versio: 2024-09-09
 
 // ASETUKSET
-let Rankit = [1, 2, 3];     // Listaa 'rankit' (eli tunnin järjestysnumero hinnan mukaan), jolloin rele kytketään
-let Hintaero = -1.43;       // Paljonko sähkön siirtohinta on halvempi yösiirron aikaan?
-let Rele = 0;               // Ohjattavan releen numero?
-let Varatunnit = [3, 4, 5]; // Tunnit jolloin rele kytketään, mikäli Internet yhteys ei toimi tai palvelu on alhaalla
+let Rankit = [1, 2, 3];  // Listaa 'rankit' (eli tunnin järjestysnumero hinnan mukaan), jolloin rele kytketään
+let Rele = 0;  // Ohjattavan releen numero
+let Yotunnit = [22, 23, 0, 1, 2, 3, 4, 5, 6]; // Yösiirron tunnit. Näihin ei tarvitse normaalisti koskea (edes kellonsiirron aikaan).
+let Hintaero = -1.43;  // Paljonko sähkön siirtohinta on halvempi yösiirron aikaan?
+let Varatunnit = [3, 4, 5];  // Tunnit jolloin rele kytketään, mikäli Internet yhteys ei toimi tai palvelu on alhaalla
 
 // KOODI
-let url = "https://api.spot-hinta.fi/JustNowRanksAndPrice?priorityHours=22,23,0,1,2,3,4,5,6&priceModifier=" + Hintaero + "&ranksAllowed=" + Rankit.join();
+let url = "https://api.spot-hinta.fi/JustNowRanksAndPrice?priorityHours=" + Yotunnit.join() + "&priceModifier=" + Hintaero + "&ranksAllowed=" + Rankit.join();
 let hour = -1; print("WaterBoiler: Ohjaus käynnistyy 30 sekunnissa.");
 Timer.set(30000, true, function () {
     if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista."); return; }
@@ -18,7 +19,7 @@ Timer.set(30000, true, function () {
         if (hour === -1) {
             if (Varatunnit.indexOf(new Date().getHours()) > -1) {
                 on = true; hour = new Date().getHours();
-                print("WaterBoiler: Virhetilanne. Kuluva tunti on varatunti: rele kytketään tämän tunnin ajaksi.");
+                print("WaterBoiler: Virhetilanne. Kuluva tunti on varatunti: rele kytketään päälle tämän tunnin ajaksi.");
             } else {
                 print("WaterBoiler: Virhetilanne. Kuluva tunti ei ole varatunti: relettä ei kytketä. Yhteyttä yritetään uudestaan.");
             }
--- Scripts/Shelly-Vesivaraaja.js
+++ Scripts/Shelly-Vesivaraaja.js
@@ -1,20 +1,29 @@
 ﻿// Kiitos tuestasi: https://www.buymeacoffee.com/spothintafi
-// Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.2. Skriptin versio: 2024-08-26
+// Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.2. Skriptin versio: 2024-09-09
 
 // ASETUKSET
-let TUNNIT_YO = 3; // Yötuntien lukumäärä 22:00 - 06:59?
-let TUNNIT_IP = 0; // Iltapäivätuntien lukumäärä 12:00 - 19:59?
-let RELE = 0;      // Mitä relettä ohjataan? 
+let Tunnit_yo = 3;          // Yötuntien lukumäärä 22:00 - 07:00
+let Tunnit_ip = 0;          // Iltapäivätuntien lukumäärä 12:00 - 20:00
+let Rele = 0;               // Ohjattavan releen numero
+let Varatunnit = [3, 4, 5]; // Tunnit jolloin rele kytketään, mikäli Internet yhteys ei toimi tai palvelu on alhaalla
 
 // KOODI
-let url = "https://api.spot-hinta.fi/WaterBoiler/" + TUNNIT_YO + "/" + TUNNIT_IP; let hour = -1;
-print("WaterBoiler: Ohjaus käynnistyy 30 sekunnissa.");
+let url = "https://api.spot-hinta.fi/WaterBoiler/" + Tunnit_yo + "/" + Tunnit_ip;
+let hour = -1; print("WaterBoiler: Ohjaus käynnistyy 30 sekunnissa.");
 Timer.set(30000, true, function () {
     if (hour == new Date().getHours()) { print("WaterBoiler: Odotetaan tunnin vaihtumista."); return; }
     Shelly.call("HTTP.GET", { url: url, timeout: 15, ssl_ca: "*" }, function (res, err) {
         hour = (err != 0 || res == null || (res.code !== 200 && res.code !== 400)) ? -1 : new Date().getHours();
-        let on = (hour > -1) ? res.code != 400 : true;
-        Shelly.call("Switch.Set", "{ id:" + RELE + ", on:" + on + "}", null, null);
-        print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä") + ((hour > -1) ? "." : " (virhetilanne)."));
+        let on = false;
+        if (hour === -1) {
+            if (Varatunnit.indexOf(new Date().getHours()) > -1) {
+                on = true; hour = new Date().getHours();
+                print("WaterBoiler: Virhetilanne. Kuluva tunti on varatunti: rele kytketään päälle tämän tunnin ajaksi.");
+            } else {
+                print("WaterBoiler: Virhetilanne. Kuluva tunti ei ole varatunti: relettä ei kytketä. Yhteyttä yritetään uudestaan.");
+            }
+        } else { if (res.code === 200) { on = true; } }
+        Shelly.call("Switch.Set", "{ id:" + Rele + ", on:" + on + "}", null, null);
+        print("WaterBoiler: Kytketty " + (on ? "päälle" : "pois päältä"));
     });
 });
