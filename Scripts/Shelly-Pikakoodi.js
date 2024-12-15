75c03b11c3de1a741cf1934e33706d0b7f755ea8
Heikki Vesalainen <heikki.vesalainen@trademarknow.com>
2024-12-15 23:40:27 +0200

    Fix `tunti` not updating if `Pikakoodit[0] == 999`

--- Scripts/Shelly-Pikakoodi.js
+++ Scripts/Shelly-Pikakoodi.js
@@ -7,24 +7,24 @@
 // Hae sopivat pikakoodit täältä: https://spot-hinta.fi/pikakoodit
 
 // PIKAKOODIT: Aseta tähän parametriin haluamasi pikakoodit
-let Pikakoodit = [103];
+let Pikakoodit = [999, 113];
 
 // SKRIPTI. Älä muokkaa alla olevaa koodia
-let rr = -1; let tunti = -1; let vtila = false; let tilat = [null, null, null, null]; print("Pikakoodi: Skripti käynnistyy.");
+let rr = 0; let tunti = -1; let vtila = false; let tilat = [null, null, null, null]; print("Pikakoodi: Skripti käynnistyy.");
 Timer.set(15000, true, function () {
     if (tunti == new Date().getHours()) { return; } else {
-        rr--; rr = (rr < 0) ? Pikakoodit.length - 1 : rr; if (Pikakoodit[rr] == 999) { return; }
+        rr = (rr == Pikakoodit.length) ? 0 : rr; if (Pikakoodit[rr] == 999) { rr++; return; }
         Shelly.call("HTTP.GET", { url: "https://api.spot-hinta.fi/QuickCode/" + Pikakoodit[rr], timeout: 10, ssl_ca: "*" }, function (res, err) {
             let virhe = (err != 0 || res == null) ? true : false; let tila = (virhe) ? true : false;
             if (!virhe) { tila = (res.code == 400) ? false : true; virhe = (res.code != 200 && res.code != 400) ? true : false; }
-            vtila = (virhe) ? true : false;
+            vtila = vtila || virhe;
             if (virhe || tila != tilat[rr]) {
                 Shelly.call("Switch.Set", "{ id:" + rr + ", on:" + tila + "}", null, null);
                 print("Pikakoodi: Rele " + rr + " on kytketty " + (tila ? "päälle" : "pois päältä") + (!virhe ? "." : " (virhetilanne)."));
                 if (virhe) { tilat[rr] = null; } else { tilat[rr] = tila; }
                 if (virhe) { print("Pikakoodi: Rele " + rr + " virheellinen vastaus verkosta: " + JSON.stringify(res)); }
-            } else { print("Pikakoodi: Rele " + rr + " tilaa ei muutettu, koska tila on sama kuin edellisellä tunnilla."); }            
-            if (rr == 0) { tunti = (vtila) ? -1 : new Date().getHours(); print("Pikakoodi: Tunti suoritettu?  " + !vtila); vtila = (tunti > -1) ? false : true; }
+            } else { print("Pikakoodi: Rele " + rr + " tilaa ei muutettu, koska tila on sama kuin edellisellä tunnilla."); }
+            if (++rr == Pikakoodit.length) { tunti = (vtila) ? -1 : new Date().getHours(); print("Pikakoodi: Tunti suoritettu?  " + !vtila); vtila = false; }
         });
     }
 });
