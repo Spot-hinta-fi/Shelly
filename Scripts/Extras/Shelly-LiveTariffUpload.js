7dc603c7e66124ef28c0f7859787a7b3a590eadf
Lehto, Mika <mikal@basware.com>
2025-01-18 16:11:59 +0200

    Added support for Area. Updated comments.

--- Scripts/Extras/Shelly-LiveTariffUpload.js
+++ Scripts/Extras/Shelly-LiveTariffUpload.js
@@ -1,16 +1,17 @@
-/* Jos haluat tukea spot-hinta.fi palvelua: https://www.buymeacoffee.com/spothintafi
-   Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.4. Skriptin versio: 2025-01-16 */
+/* Jos haluat tukea spot-hinta.fi palvelua: www.buymeacoffee.com/spothintafi
+   Tuetut Shelly ohjelmistot: 1.0.3 - 1.4.4. Skriptin versio: 2025-01-18 */
 
 // Asetukset
 const ApiUrl = ""; // Shelly Control sivustolta haettu Live Tariff osoite (https://shelly...)
 const PriceNight = 0.0112; // Siirtohinta yöaikaan euroina (ilman veroa) (oletus: Caruna Espoo yösiirto)
 const PriceDay = 0.0255;   // Siirtohinta päiväaikaan euroina (ilman veroa) (oletus: Caruna Espoo yösiirto)
-const Margin = 0.00456;     // Pörssisähkön myyjän ottama toimitusmaksu/marginaali (oletus: PKS Priima) 
-const Tax = 0.02827515;    // Vero (sis. ALV 25.5%). Muuta vain jos laki muuttuu.
+const Margin = 0.00456;    // Pörssisähkön myyjän ottama toimitusmaksu/marginaali (oletus: PKS Priima) 
+const Tax = 0.02827515;    // Sähkövero. Muuta vain jos laki muuttuu (oletus: vero Suomessa)
+const Area = "FI";         // Alue jolle hinta haetaan FI, SEx, NOx, DKx, EE, LV, LT. Hinta on aina euroina.
 
 // Scripti
 let minNow = 0; let minDone = -1; let tra = 0; let started = true; const uMins = [0, 15, 30, 45]; const fName = "LiveTariffUpload";
-const nHours = [22, 23, 0, 1, 2, 3, 4, 5, 6]; const pUrl = "https://api.spot-hinta.fi/JustNowPrice";
+const nHours = [22, 23, 0, 1, 2, 3, 4, 5, 6]; const pUrl = "https://api.spot-hinta.fi/JustNowPrice?region=" + Area;
 print(fName + ": Skripti käynnistyi. Ensimmäinen hintojen lähetys tehdään 15 sekunnin kuluttua.");
 Timer.set(15000, true, function () {
     minNow = new Date().getMinutes(); tra = (nHours.indexOf(new Date().getHours()) > -1) ? PriceNight : PriceDay;
